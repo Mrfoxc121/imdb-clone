4ce66c1c5c29ff6bc27b6ccb98e62907dbea58b9
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-08-02 09:08:59 +0100

    create next app and component folders and files

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import {HomeIcon} from '@heroicons/react'
+
+
+export default function Header() {
+    return (
+    <div>
+        <h1>Header</h1>
+    </div>
+    )
+}
+
---
+++ components/HeaderIcpon.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HeaderIcpon() {
+  return (
+    <div>
+      <h1>HeaderIcpon</h1>
+    </div>
+  );
+}
