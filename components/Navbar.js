2c291ca9df6c09b0db65f036b175c1004b56dcc5
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-08-02 20:26:22 +0100

    create navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,19 @@
+import Requests from "../utils/Requests";
+import { useRouter } from "next/router";
+
 export default function Navbar() {
+  const router = useRouter()
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="flex justify-center bg-gray-600 text-gray-200 p-8 select-none text-xl lg:text-2xl">
+      {Object.entries(Requests).map(([key, { title, url }]) => (
+        <h2
+          onClick={() => router.push(`?genre=${key}`)}
+          className="m-6 cursor-pointer text-gray-300 hover:text-gray-100 active:text-red-400"
+          key={key}
+        >
+          {title}
+        </h2>
+      ))}
     </div>
   );
 }
--- utils/requests.js
+++ utils/requests.js
@@ -3,11 +3,11 @@ const API_KEY = process.env.API_KEY
 export default {
     fetchTrending: {
         title: "Trending",
-        url: `/trending/all/week?api_key=${api_key}&launguages=en-US`
+        url: `/trending/all/week?api_key=${API_KEY}&launguages=en-US`
     },
     fetchTopRated: {
         title: "Top Rated",
         url: `/movie/top_rated?api_key=${API_KEY}&languages+en=US`
     },
+    
 }
-
